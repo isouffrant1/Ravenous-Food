25aa68d7c2e341c1ed78a3fd03a4ce9209902925
isouffrant1 <60717996+isouffrant1@users.noreply.github.com>
2023-01-10 12:43:36 -0500

    Update Business.js

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -13,12 +13,12 @@ class Business extends React.Component {
                     <div className="Business-address">
                         <p>{this.props.business.address}</p>
                         <p>{this.props.business.city}</p>
-                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                        <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{this.props.business.category}</h3>
-                        <h3 className="rating">{this.props.business.rating} stars</h3>
-                        <p>{this.props.business.reviewCount} reviews</p>
+                        <h3>{this.props.business.category.toUpperCase()}</h3>
+                        <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+                        <p>{`${this.props.business.reviewCount} reviews`}</p>
                     </div>
                 </div>
             </div>
