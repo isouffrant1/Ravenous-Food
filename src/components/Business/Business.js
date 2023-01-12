e0deeca646f207b2a75c1308f7c32858f48af9cf
isouffrant1 <60717996+isouffrant1@users.noreply.github.com>
2023-01-11 20:21:14 -0500

    Update Business.js

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -6,7 +6,7 @@ class Business extends React.Component {
         return (
            <div className="Business">
                 <div className="image-container">
-                    <img src='https://content.codecademy.com/programs/react/ravenous/pizza.jpg' alt=''/>
+                    <img src={this.props.business.imageSrc} alt=''/>
                 </div>
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
